cbcfd054277b2881356dbb2ba0dc58ee82b1a81e
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-01-18 21:17:04 +0530

    redis created

---
+++ src/configs/redis.js
@@ -0,0 +1,11 @@
+const redis = require("redis"); ;
+
+const client = redis.createClient();
+
+client.on("error", function (err) { 
+    console.log(err);
+})
+
+module.exports = client
+
+
--- src/controllers/foodItems.controller.js
+++ src/controllers/foodItems.controller.js
@@ -1,38 +1,48 @@
 const router = require("express").Router();
 // const fooditems = require("./fooditems.json");
 const Product = require("../models/product.model")
+const redis = require("../configs/redis")
 
 //localhost:3000/foodItems
-http: router.get("", async (req, res) => {
+// router.get("", async (req, res) => {
+//   try {
+//     const products = await Product.find().sort().lean().exec();
+//     res.send({ items: products });
+//   } catch (err) {
+//     res.send(err);
+//   }
+// });
+
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const product = await Product.find({ _id: req.params.id }).lean().exec();
+//     res.send( product);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// })
+
+
+//    ######################################### Redis methode #################
+router.get("", async (req, res) => {
   try {
-    const products = await Product.find().lean().exec();
-    res.send({ items: products });
+    redis.get("products", async function (err, products) {
+      if (err) return res.status(500).send({ message: err.message });
+
+      if (products) {
+        const allPorducts = JSON.parse(products);
+        return res.status(200).send({ products: allPorducts, redis: true });
+      } else {
+        const products = await Product.find().sort().lean().exec();
+        redis.set("products", JSON.stringify(products));
+        res.send({ products: products, redis: false });
+      }
+    });
   } catch (err) {
-    res.send(err);
+    res.send(err.message);
   }
 });
 
 
-//    ######################################### Redis methode #################
-// router.get("", async (req, res) => {
-//   try {
-//     redis.get("products", async function (err, products) {
-//       if (err) return res.status(500).send({ message: err.message });
 
-//       if (products) {
-//         const allPorducts = JSON.parse(products);
-//         return res.status(200).send({ products: allPorducts, redis: true });
-//       } else {
-//         const products = await Product.find()
-//           .sort({ price: "-1" })
-//           .lean()
-//           .exec();
-//         redis.set("products", JSON.stringify(products));
-//         res.send({ products: products, redis: false });
-//       }
-//     });
-//   } catch (err) {
-//     res.send(err.message);
-//   }
-// });
 module.exports = router;
