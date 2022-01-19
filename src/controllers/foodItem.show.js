2cf4f86b8d44ea1e09917599858f70d1eeab5212
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-01-19 13:15:31 +0530

    collections page

--- src/controllers/foodItem.show.js
+++ src/controllers/foodItem.show.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 const Product = require("../models/product.model")
+
 router.get("", async (req, res) => {
     const products = await Product.find().lean().exec()
     // console.log(products)
     res.render("collections", {products:products});
 });
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.find({ _id: req.params.id }).lean().exec();
+    res.send({ item: product });
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 
 module.exports = router;
