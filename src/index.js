48d79b17a06e47956ad68def2ed65230ebb3d56b
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-01-23 10:40:54 +0530

    public path change

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.set("view engine", "ejs"); // root directory for views views/
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
